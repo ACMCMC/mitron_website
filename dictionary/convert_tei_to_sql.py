3654160131bb731bd6e6fa25a892be937b5ab6d7
Aldan Creo <20495460+ACMCMC@users.noreply.github.com>
2020-08-10 20:05:35 +0200

    Draft script to upload files, to be improved

--- dictionary/convert_tei_to_sql.py
+++ dictionary/convert_tei_to_sql.py
@@ -1,6 +1,47 @@
 import xmltodict
+import pprint
 
-with open('/Users/agustinsuarez/Documents/swe-spa.tei') as fd:
+with open('REDACTED', encoding="utf8") as fd:
     doc = xmltodict.parse(fd.read())
 
-print(doc['TEI']['text']['body']['entry'][1])
+doc_scan = doc['TEI']['text']['body']['entry']
+
+lista_terms_spa = [entry['form']['orth'] for entry in doc_scan]
+
+with open('REDACTED', encoding="utf8") as fd:
+    doc = xmltodict.parse(fd.read())
+
+doc_scan = doc['TEI']['text']['body']['entry']
+
+lista_fin = [(entry['form']['orth'], entry['gramGrp']['pos'], entry['sense'] ) for entry in doc_scan]
+
+for i in range(len(lista_fin)):
+    if isinstance(lista_fin[i][2], list):
+        flatten_list = [thing['cit']['quote'] for thing in lista_fin[i][2]]
+    else:
+        flatten_list = [lista_fin[i][2]['cit']['quote']]
+    flat = [item for sublist in flatten_list for item in (sublist if isinstance(sublist, list) else [sublist])]
+    lista_fin[i] = (lista_fin[i][0], lista_fin[i][1], flat)
+
+lista_fin = list(filter(lambda x: any([True if y in lista_terms_spa else False for y in x[2]]), lista_fin))
+
+print(lista_fin[0:10], len(lista_fin))
+types_words_list = list(map(lambda x: x.upper(), set([elem[1] for elem in lista_fin])))
+print(types_words_list)
+print(len(max(types_words_list, key=len)))
+print(max(lista_fin, key=(lambda x: len(x[2][0]))))
+
+lista_fin = list(map(lambda x: (x[0], 'NOUN' if x[1] == 'n' else 'ADVERB' if x[1] == 'adv' else 'VERB' if x[1] == 'v' else 'ADJECTIVE' if x[1] == 'adj' else x[1].upper(), x[2]), lista_fin))
+
+
+import psycopg2
+conn = psycopg2.connect("REDACTED")
+
+cur = conn.cursor()
+
+for i in range(len(lista_fin)):
+    cur.execute("INSERT INTO dictionary_word (mitronese_word, word_class, translation) VALUES (%s, %s, %s)", (lista_fin[i][0],lista_fin[i][1],lista_fin[i][2][0]))
+    print(i)
+conn.commit()
+cur.close()
+conn.close()
--- dictionary/models.py
+++ dictionary/models.py
@@ -9,13 +9,11 @@ class OriginLanguage(models.Model):
     def __str__(self):
         return self.name
 
-
 class Word(models.Model):
-    mitronese_word = models.CharField(max_length=30)
-    spanish_word = models.CharField(max_length=30)
-    WORD_CLASSES = [(word[2], word) for word in ["Nombre", "Pronombre", "Verbo",
-                                                 "Adjetivo", "Adverbio", "Preposición", "Conjunción", "Interjección"]]
-    word_class = models.CharField(max_length=1, choices=WORD_CLASSES)
+    mitronese_word = models.CharField(max_length=40)
+    translation = models.CharField(max_length=40)
+    WORD_CLASSES = [(word.upper(), word) for word in ['AFFIX', 'NOUN', 'PROVERB', 'POSTPOSITION', 'VERB', 'ADJECTIVE', 'ARTICLE', 'PRONOUN', 'PREPOSITION', 'ADVERB', 'INTERJECTION', 'CONJUNCTION']]
+    word_class = models.CharField(max_length=15, choices=WORD_CLASSES)
     orig_language = models.ManyToManyField(OriginLanguage)
 
     def __str__(self):
